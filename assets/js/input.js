89cdc3309e56ca92dfc8d3a9e2936473107405e1
Hans Lemuet <hans@etaminstudio.com>
2016-11-22 02:39:17 +0100

    Added options

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,7 +1,7 @@
 (function($){
 
 
-	function initialize_field( $el ) {
+  function initialize_field( $el ) {
 
     var $hiddenField = $el.find('input[type=hidden]');
     var $searchField = $el.find('input[type=search]');
@@ -14,9 +14,19 @@
 
     $searchField.val(data.value);
 
-    var placesAutocomplete = places({
+    var options = {
       container: $searchField[0]
-    });
+    };
+
+    if ($hiddenField.data('algolia-type')) {
+      options.type = $hiddenField.data('algolia-type');
+    }
+
+    if ($hiddenField.data('algolia-countries')) {
+      options.countries = $hiddenField.data('algolia-countries').split(',');
+    }
+
+    var placesAutocomplete = places(options);
 
     placesAutocomplete.on('change', function(e){
       var s = e.suggestion;
@@ -33,70 +43,71 @@
         postcode: s.postcode,
         value: s.value
       };
+
       $hiddenField.val(JSON.stringify(json));
     });
 
-	}
+  }
 
 
-	if( typeof acf.add_action !== 'undefined' ) {
+  if( typeof acf.add_action !== 'undefined' ) {
 
-		/*
-		*  ready append (ACF5)
-		*
-		*  These are 2 events which are fired during the page load
-		*  ready = on page load similar to $(document).ready()
-		*  append = on new DOM elements appended via repeater field
-		*
-		*  @type	event
-		*  @date	20/07/13
-		*
-		*  @param	$el (jQuery selection) the jQuery element which contains the ACF fields
-		*  @return	n/a
-		*/
+    /*
+    *  ready append (ACF5)
+    *
+    *  These are 2 events which are fired during the page load
+    *  ready = on page load similar to $(document).ready()
+    *  append = on new DOM elements appended via repeater field
+    *
+    *  @type  event
+    *  @date  20/07/13
+    *
+    *  @param  $el (jQuery selection) the jQuery element which contains the ACF fields
+    *  @return  n/a
+    */
 
-		acf.add_action('ready append', function( $el ){
+    acf.add_action('ready append', function( $el ){
 
-			// search $el for fields of type 'algolia_places'
-			acf.get_fields({ type : 'algolia_places'}, $el).each(function(){
+      // search $el for fields of type 'algolia_places'
+      acf.get_fields({ type : 'algolia_places'}, $el).each(function(){
 
-				initialize_field( $(this) );
+        initialize_field( $(this) );
 
-			});
+      });
 
-		});
-
-
-	} else {
+    });
 
 
-		/*
-		*  acf/setup_fields (ACF4)
-		*
-		*  This event is triggered when ACF adds any new elements to the DOM.
-		*
-		*  @type	function
-		*  @since	1.0.0
-		*  @date	01/01/12
-		*
-		*  @param	event		e: an event object. This can be ignored
-		*  @param	Element		postbox: An element which contains the new HTML
-		*
-		*  @return	n/a
-		*/
-
-		$(document).on('acf/setup_fields', function(e, postbox){
-
-			$(postbox).find('.field[data-field_type="algolia_places"]').each(function(){
-
-				initialize_field( $(this) );
-
-			});
-
-		});
+  } else {
 
 
-	}
+    /*
+    *  acf/setup_fields (ACF4)
+    *
+    *  This event is triggered when ACF adds any new elements to the DOM.
+    *
+    *  @type  function
+    *  @since  1.0.0
+    *  @date  01/01/12
+    *
+    *  @param  event    e: an event object. This can be ignored
+    *  @param  Element    postbox: An element which contains the new HTML
+    *
+    *  @return  n/a
+    */
+
+    $(document).on('acf/setup_fields', function(e, postbox){
+
+      $(postbox).find('.field[data-field_type="algolia_places"]').each(function(){
+
+        initialize_field( $(this) );
+
+      });
+
+    });
+
+
+  }
 
 
 })(jQuery);
